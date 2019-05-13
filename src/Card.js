47d539cbd5e7c1ac4b734e4d12052fa8282f9b1d
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-13 12:55:09 +0530

    progress

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-function App () {
+
+function Card () {
     return(
-        <div className = "card">
-            <div class="photo"></div>
-            <div class="name"></div>
-            <dic class="desc"></dic>
-        </div>
+      <div class="card"> </div>
     )
 };
 
 
-export default App;
+export default Card;
