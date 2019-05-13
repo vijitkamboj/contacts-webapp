6bff799663884b58f22b373cfec12ac4afd038d2
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-13 21:13:46 +0530

    Reset Everything to start excepr layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './index.css';
-import Results from './results';
-import * as serviceWorker from './serviceWorker';
-
-var res = document.getElementById('app-cont')
-ReactDOM.render(<Results />, res,);
+// import * as serviceWorker from './serviceWorker';
 
 
-serviceWorker.register();
+
+// serviceWorker.register();
