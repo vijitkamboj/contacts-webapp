d2568fb97e98435b80033c97e814812fd080cf6c
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-16 03:44:16 +0530

    Made eveything awesome

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -3,7 +3,12 @@ import './footer.css';
 
 function app(){
     return(
-        <div id="footer"></div>
+        <div id="footer">
+            <a href="https://www.facebook.com/vijitkamboj945" target="_blank"><div id="fb" class="icon"></div></a>
+            <a href="https://www.instagram.com/kambojvijit" target="_blank"><div id="ins" class="icon"></div></a>
+            <a href="https://www.linkedin.com/in/vijit-kamboj-416311147" target="_blank"><div id="ln" class="icon"></div></a>
+            <a href="https://github.com/vijitkamboj/" target="_blank"><div id="git" class="icon"></div></a>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css'
 import Nav from './Nav/nav';
 import Cover from './Cover/cover';
 import App from './App/app';
+import Footer from './Footer/footer';
+
 
 
 ReactDOM.render(<div id="container">
                             <Cover />
                             <App />
                             <Nav />
+                            <Footer />
                             </div> 
                             , document.getElementById('root'));
 
