a3d9509920107e208ff3a4300df0e111779106d6
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-16 02:06:33 +0530

    added quote

--- src/Cover/cover.js
+++ src/Cover/cover.js
@@ -3,7 +3,9 @@ import './cover.css';
 
 function app(){
     return(
-        <div id="cover"></div>
+        <div id="cover"><div id="quote">
+            Spend less time looking for contacts and more time connecting with people
+        </div></div>
     )
 }
 
--- src/details.js
+++ src/details.js
@@ -1,33 +1,63 @@
-var contacts = {
+var contacts = [
 
-    srijan: {
+    {
         firstName: "Srijan",
         lastName: "Kamboj",
         number: "07872937209",
-
     },
-    kuldeep: {
+    {
         firstName: "Kuldeep",
         lastName: "Singh",
         number: "07970943757",
     },
-    vijit: {
+    {
         firstName: "Vijit",
         lastName: "Kamboj",
         number: "8769957579"
     },
 
-    nikhil: {
+    {
         firstName: "Nikhil",
         lastName: "Chauhan",
         number: "9283479825"
     },
-    raunak: {
+    {
         firstName: "Raunak",
         lastName: "Negi",
+        number: '9342342342'
+    },
+    {
+        firstName: "Shivnsh",
+        lastName: "Dwivedi",
+        number: '9342342342'
+    },
+    {
+        firstName: "Narendra",
+        lastName: "Singh",
+        number: '9342342342'
+    },
+    {
+        firstName: "Navdeep",
+        lastName: "Kamboj",
+        number: '9342342342'
+    },
+    {
+        firstName: "Saanvi",
+        lastName: "Kamboj",
+        number: '9342342342'
+    },
+    {
+        firstName: "Onkar",
+        lastName: "Kamboj",
+        number: '9342342342'
+    },
+    {
+        firstName: "Somil",
+        lastName: "Kamboj",
         number: '9342342342'
     },
 
 
 
-};
+
+];
