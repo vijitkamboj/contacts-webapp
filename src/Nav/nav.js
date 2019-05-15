8f9174ad55fffc66b947a97a341d4ebafee2bfa9
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-15 22:22:44 +0530

    fixed navbar classes  deleted extra assets

--- src/Nav/nav.js
+++ src/Nav/nav.js
@@ -5,7 +5,7 @@ function app(){
     return(
         <div id="nav">
             <div className="navbtn">About</div>
-            <div className="navbtn"><a href="https://github.com/vijitkamboj/" id="nav">GitHub</a></div>
+            <div className="navbtn"><a href="https://github.com/vijitkamboj/" className="link">GitHub</a></div>
             <div className="navbtn">Log In</div>
             <div className="navbtn" id="spcbtn">Sign Up</div>
 	    </div>
