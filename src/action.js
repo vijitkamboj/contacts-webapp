01fcb2cdc80b9e010283c246d57b444883c317c1
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-18 00:18:37 +0530

    update

--- src/action.js
+++ src/action.js
@@ -1,5 +1,4 @@
- import Constants from './constants'
- const {CHANGE_SEARCH_FIELD} = Constants;
+ import {CHANGE_SEARCH_FIELD} from './constants'
 
 export const setSearchField = (text) => ({
     type : CHANGE_SEARCH_FIELD , 
